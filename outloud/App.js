54007e1c97a8a726d9515873a51101f80ff3c1de
Marcin Miazga <miazgamarcin@outlook.com>
2018-09-22 16:45:26 +0200

    add auth component

--- outloud/App.js
+++ outloud/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import AuthComponent from './src/components/AuthComponent'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -13,9 +14,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <AuthComponent/>
       </View>
     );
   }
