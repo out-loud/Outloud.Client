7f88a72e7906eb65ca8877c8339909a1955f1278
Marcin Miazga <miazgamarcin@outlook.com>
2018-11-20 20:54:05 +0100

    add some fonts

--- outloud/android/app/src/main/java/com/outloud/MainApplication.java
+++ outloud/android/app/src/main/java/com/outloud/MainApplication.java
@@ -3,6 +3,7 @@ package com.outloud;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.wenkesj.voice.VoicePackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import io.sentry.RNSentryPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeAudioPackage(),
             new VoicePackage(),
             new RNSoundPackage(),
             new RNSentryPackage(),
